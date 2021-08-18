8dce19718a3dea79bf94b5561e9f3278e9636950
Louis <admin@louistechnology.com>
2021-08-19 03:58:25 +0800

    remove unsed code

--- src/components/PostList/index.tsx
+++ src/components/PostList/index.tsx
@@ -19,19 +19,6 @@ import {
     StyledPaginationPrevDisabledBtn
 } from './styles';
 
-const categoryList: ICategory[] = [
-    { id: "c63bf3b1-06bd-4499-ab11-93b147ec948d", name: "Data Management" },
-    { id: "7b28d5c0-0853-406f-b115-bd14c1ae683f", name: "Digital Marketing" },
-    { id: "a1c141f7-0d0e-4521-ba2b-63d1265dcca1", name: "Ecommerce" },
-    { id: "0f91bf2c-aa47-4372-8c22-b403899751b7", name: "Email Marketing" },
-    { id: "fd738de2-aa84-436b-9282-ce5f6944dab7", name: "Landing Pages" },
-    { id: "1ce0ff98-b6c3-4f9a-bc3a-a7dd41e8c471", name: "Marketing Analytics" },
-    { id: "b17d04b7-57d8-4a37-bc76-343699cdb113", name: "Marketing Automation" },
-    { id: "d8521992-c9ff-4e5e-bbb1-7715d8bdf833", name: "Platform News and Updates" },
-    { id: "0e1eeaa5-9600-41b5-ade9-12c01efe0362", name: "Surveys and Forms" },
-    { id: "41f72a87-5927-403d-89ce-216d325daa40", name: "Tips and Best Practise" },
-];
-
 export const PostList: React.FC = () => {
     const PAGE_SIZE = 5;
     
